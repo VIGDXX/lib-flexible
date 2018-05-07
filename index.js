f7dbe1fd1bbcad6961d00d2fa761aaf9e8c9790d
VIGDXX <VIGDXX@gmail.com>
2018-05-07 15:00:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
 
   // set 1rem = viewWidth / 10
   function setRemUnit () {
-    var rem = docEl.clientWidth / 10
+    var rem = docEl.clientWidth / 3.75
     docEl.style.fontSize = rem + 'px'
   }
 
